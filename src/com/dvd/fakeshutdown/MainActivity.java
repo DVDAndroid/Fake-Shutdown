97b8db9ab0073b781c83be072bd529f73a93bfbb
DVDAndroid <davide.intelligent@gmail.com>
2014-09-11 10:54:31 +0200

    add support for android L

--- src/com/dvd/fakeshutdown/MainActivity.java
+++ src/com/dvd/fakeshutdown/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -40,89 +41,39 @@ public class MainActivity extends Activity {
 
 		boolean active = deviceManger.isAdminActive(compName);
 		if (active) {
-			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-					MainActivity.this);
+			if (Build.VERSION.SDK_INT == 20) {
+				start();
+			} else {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						MainActivity.this);
 
-			alertDialogBuilder.setTitle(getApplicationContext().getString(
-					R.string.app_name));
-			alertDialogBuilder.setMessage(getApplicationContext().getString(
-					R.string.warn));
+				alertDialogBuilder.setTitle(getApplicationContext().getString(
+						R.string.app_name));
+				alertDialogBuilder.setMessage(getApplicationContext()
+						.getString(R.string.warn));
 
-			alertDialogBuilder.setPositiveButton(android.R.string.ok,
-					new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int id) {
+				alertDialogBuilder.setPositiveButton(android.R.string.ok,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int id) {
 
-							progressDialog = new ProgressDialog(
-									MainActivity.this);
-							progressDialog.setCancelable(true);
-							progressDialog
-									.setTitle(getString(R.string.title_activity_shutdown));
-							progressDialog
-									.setMessage(getString(R.string.descr));
-							progressDialog
-									.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-							progressDialog.setCancelable(false);
-							progressDialog.setProgress(0);
-							progressDialog.setMax(100);
-							progressDialog.show();
-							progressStatus = 0;
-							new Thread(new Runnable() {
+								start();
 
-								@Override
-								public void run() {
-									// TODO Auto-generated method stub
-									while (progressStatus < 10) {
-										progressStatus = performTask();
+							}
+						});
 
-									}
-									/* Dismiss the Progress bar */
-									myHandler.post(new Runnable() {
+				alertDialogBuilder.setNegativeButton(android.R.string.cancel,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int id) {
+								finish();
+							}
+						});
 
-										@Override
-										public void run() {
-											// TODO Auto-generated method stub
-											progressDialog.dismiss();// dismiss
-																		// the
-																		// dialog
-
-											progressStatus = 0;
-											myProgress = 0;
-											deviceManger = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-											deviceManger.lockNow();
-
-											finish();
-
-										}
-									});
-
-								}
-
-								/* Do some task */
-								private int performTask() {
-									try {
-										// ---simulate doing some work---
-										Thread.sleep(600);
-									} catch (InterruptedException e) {
-										e.printStackTrace();
-									}
-									return ++myProgress;
-								}
-							}).start();
-						}
-					});
-
-			alertDialogBuilder.setNegativeButton(android.R.string.cancel,
-					new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int id) {
-							finish();
-						}
-					});
-
-			AlertDialog alertDialog = alertDialogBuilder.create();
-			alertDialog.setCancelable(false);
-			alertDialog.show();
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				alertDialog.setCancelable(false);
+				alertDialog.show();
+			}
 		}
 
 		else {
@@ -137,12 +88,62 @@ public class MainActivity extends Activity {
 			mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000,
 					RESTART_INTENT);
 			System.exit(2);
-
-			// Toast.makeText(getApplicationContext(), R.string.acc_grant,
-			// Toast.LENGTH_SHORT).show();
 			finish();
 		}
 
 	}
 
+	public void start() {
+		progressDialog = new ProgressDialog(MainActivity.this);
+		progressDialog.setCancelable(true);
+
+		if (!(Build.VERSION.SDK_INT == 20)) {
+			progressDialog
+					.setTitle(getString(R.string.title_activity_shutdown));
+		}
+
+		progressDialog.setMessage(getString(R.string.descr));
+		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		progressDialog.setCancelable(false);
+		progressDialog.setProgress(0);
+		progressDialog.setMax(100);
+		progressDialog.show();
+		progressStatus = 0;
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (progressStatus < 10) {
+					progressStatus = performTask();
+
+				}
+				myHandler.post(new Runnable() {
+
+					@Override
+					public void run() {
+
+						progressDialog.dismiss();
+
+						progressStatus = 0;
+						myProgress = 0;
+						deviceManger = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+						deviceManger.lockNow();
+
+						finish();
+
+					}
+				});
+
+			}
+
+			private int performTask() {
+				try {
+					Thread.sleep(600);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				return ++myProgress;
+			}
+		}).start();
+	}
 }
